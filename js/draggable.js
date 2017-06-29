9bbb8fc1d6923cef4402dd25249f635dfb33cb0b
zhuifengdetianye <nianhuawuxiaoxin@yeah.net>
2017-06-29 20:29:40 +0800

    注销draggable和sortable的实现

--- js/draggable.js
+++ js/draggable.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    /*
     $('.linear-layout-module').draggable({
         helper: 'clone'
     });
@@ -47,4 +48,5 @@ $(document).ready(function(){
             });
         }
     });
+    */
 })
