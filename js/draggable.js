9067471b643af1c669daafc1eedfcea601853f62
zhuifengdetianye <nianhuawuxiaoxin@yeah.net>
2017-07-08 22:08:17 +0800

    实现行容器，子容器，栏目容器之间的相互拖拽排序

--- js/draggable.js
+++ js/draggable.js
@@ -1,24 +1,79 @@
 $(document).ready(function(){
+	//注册行容器的拖拽事件
     $(".linear-layout-module").draggable({
 		connectToSortable: "#operationPanelContainer",
 		helper: "clone",
-		start: function(e,t) {
+		start: function(e, t) {
 			
 		},
 		drag: function(e, t) {
 			
 		},
 		stop: function(e, t) {
-			$("#operationPanelContainer").sortable({
+			//注册行容器之间的拖拽排序事件
+			$(".linear-layout").sortable({
 				opacity: .35,
-				connectWith: "#operationPanelContainer",
+				connectWith: ".linear-layout",
 				start: function(e,t) {
 					
 				},
 				stop: function(e,t) {
-					
+					//注册子容器之间的拖拽排序事件
+					$(".sub-container").sortable({
+						opacity: .35,
+						connectWith: ".sub-container",
+						start: function(e,t) {
+							
+						},
+						stop: function(e,t) {
+							
+						}
+					});
 				}
 			});
 		}
 	});
+
+	//操作面板的sortable事件
+	$("#operationPanelContainer").sortable({
+		opacity: .35,
+		connectWith: "#operationPanelContainer",
+		start: function(e,t) {
+			
+		},
+		stop: function(e,t) {
+			
+		}
+	});
+
+	//注册子容器的拖拽事件
+	$(".sub-container-module").draggable({
+		connectToSortable: ".linear-layout",
+		helper: "clone",
+		start: function(e, t) {
+
+		},
+		drag: function(e, t) {
+			
+		},
+		stop: function(e, t) {
+			
+		}
+	});
+
+	//注册栏目容器的拖拽事件
+	$(".column-container-module").draggable({
+		connectToSortable: ".sub-container",
+		helper: "clone",
+		start: function(e, t) {
+
+		},
+		drag: function(e, t) {
+			
+		},
+		stop: function(e, t) {
+			
+		}
+	});
+
 })
